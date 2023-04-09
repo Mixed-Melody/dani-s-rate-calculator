3180a5ddb7a53374b765c4346643cd936ca9e162
Mixed-Melody <130159680+Mixed-Melody@users.noreply.github.com>
2023-04-08 21:14:51 -0500

    update

--- src/script.js
+++ src/script.js
@@ -1,10 +1,33 @@
 let rate;
+let totalF;
+
+function findNewTotal() {
+  const originalRate = document.getElementById("oRate");
+  const oRate = parseFloat(originalRate.value);
+  const exTaxInput = document.getElementById("exTax");
+  const eTax = parseFloat(exTaxInput.value) || 5.75;
+  document.getElementById("copy-error").textContent = "";
+  if (isNaN(oRate)) {
+    document.getElementById("oRate-error").textContent =
+      "Please enter a valid rate.";
+    totalInput.focus();
+    return;
+  } else {
+    document.getElementById("oRate-error").textContent = "";
+  }
+
+  exTaxInput.value = eTax.toFixed(2);
+  totalF = (oRate + (oRate * eTax) / 100).toFixed(2);
+  document.getElementById("total").value = totalF;
+  document.getElementById("eTotal").value = totalF;
+}
 
 function solveForRate() {
   const totalInput = document.getElementById("total");
   const total = parseFloat(totalInput.value);
   const percentInput = document.getElementById("percent-increase");
   const percent = parseFloat(percentInput.value) || 12.63;
+  document.getElementById("copyR-error").textContent = "";
 
   if (isNaN(total)) {
     document.getElementById("total-error").textContent =
@@ -20,19 +43,31 @@ function solveForRate() {
   rate = (total / (1 + percent / 100)).toFixed(2);
   document.getElementById("rate").value = rate;
 }
-function copyToClipboard() {
-  const rateField = document.getElementById("rate");
-  rateField.select();
-  rateField.setSelectionRange(0, 99999);
-  document.execCommand("copy");
+
+function copyToClipboard(s) {
+  if (s == "rate") {
+    const rateField = document.getElementById("rate");
+    rateField.select();
+    rateField.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.getElementById("copyR-error").textContent = "Rate copied.";
+    totalInput.focus();
+  }
+  if (s == "eTotal") {
+    const totalField = document.getElementById("eTotal");
+    totalField.select();
+    totalField.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.getElementById("copy-error").textContent = "Total copied.";
+    totalInput.focus();
+  }
 }
+const copyEtotalButton = document.getElementById("copy-eTotal");
+copyEtotalButton.addEventListener("click", function () {
+  copyToClipboard("eTotal");
+});
 
 const copyRateButton = document.getElementById("copy-rate");
 copyRateButton.addEventListener("click", function () {
-  if (rate) {
-    copyToClipboard();
-    alert("Rate copied.");
-  } else {
-    alert("Please calculate rate before copying.");
-  }
+  copyToClipboard("rate");
 });

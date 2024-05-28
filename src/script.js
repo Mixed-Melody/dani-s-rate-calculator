76098600ea4f6b24a29789dce49cd7d77a6d8e3f
Mixed-Melody <130159680+Mixed-Melody@users.noreply.github.com>
2024-05-28 07:27:59 -0500

    Update 5

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 let rate;
-let totalF;
 let oRate;
+let days;
 
 function findNewTotal() {
   const originalRate = document.getElementById("oRate");
   oRate = parseFloat(originalRate.value);
-  const eTax = 5.75;
+
+  const daysInput = document.getElementById("days");
+  days = parseInt(daysInput.value);
+
   document.getElementById("copy-error").textContent = "";
   if (isNaN(oRate)) {
     document.getElementById("oRate-error").textContent =
@@ -14,24 +17,10 @@ function findNewTotal() {
   } else {
     document.getElementById("oRate-error").textContent = "";
   }
-  //totalF = oRate + (oRate * 5.75) / 100;
-  // let roundedTotal = roundUp(totalF);
-
-  rate = oRate + oRate * 0.05; // (1 + 0.126314);
+  rate = (oRate + oRate * 0.05) * days;
   let roundedRate = rate.toFixed(2);
 
   document.getElementById("rate").value = roundedRate;
-  //document.getElementById("eTotal").value = roundedTotal;
-}
-
-function roundUp(value) {
-  const roundedValue = Math.round(value * 100) / 100; // Round down to second decimal
-
-  if (value - roundedValue >= 0.005) {
-    return roundedValue - 0.01; // Round up if the difference is 0.005 or //higher
-  }
-
-  return roundedValue;
 }
 
 function copyToClipboard(s) {
@@ -40,20 +29,9 @@ function copyToClipboard(s) {
     rateField.select();
     rateField.setSelectionRange(0, 99999);
     document.execCommand("copy");
-    document.getElementById("copy-error").textContent = "Rate copied.";
-  }
-  if (s == "eTotal") {
-    const totalField = document.getElementById("eTotal");
-    totalField.select();
-    totalField.setSelectionRange(0, 99999);
-    document.execCommand("copy");
     document.getElementById("copy-error").textContent = "Total copied.";
   }
 }
-//const copyEtotalButton = document.getElementById("copy-eTotal");
-//copyEtotalButton.addEventListener("click", function () {
-// copyToClipboard("eTotal");
-//});
 
 const copyRateButton = document.getElementById("copy-rate");
 copyRateButton.addEventListener("click", function () {
@@ -65,8 +43,3 @@ const rateInput = document.getElementById("oRate");
 rateInput.addEventListener("wheel", function (event) {
   event.preventDefault();
 });
-
-/*const findButton = document.getElementById("find");
-findButton.addEventListener("click", function () {
-  findNewTotal();
-});*/
